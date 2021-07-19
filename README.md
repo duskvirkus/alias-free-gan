612a1679ceceef80e0dd5e4008f2a05a3ed594ec
duskvirkus <duskvirkus@protonmail.com>
2021-07-19 04:29:53 +0000

    clean up, really should work

--- README.md
+++ README.md
@@ -1,10 +1,8 @@
 # Alias-Free GAN Pytorch Lightning
 
+![example workflow](https://github.com/duskvirkus/alias-free-gan-pytorch-lightning/actions/workflows/ci-gpu.yml/badge.svg)
+
 This is a fork of Kim Seonghyeon's (rosinality) implementation of Alias-Free Generative Adversarial Networks (https://arxiv.org/abs/2106.12423). This version has been adapted to pytorch lightning. The result can be used on a wider variety of hardware including TPUs.
-
-CI Tests:
-
-![example workflow](https://github.com/duskvirkus/alias-free-gan-pytorch-lightning/actions/workflows/ci.yml/badge.svg)
 
 ⚠️ Incomplete Project ⚠️
 
--- test/unit/alias_free_gan_test.py
+++ test/unit/alias_free_gan_test.py
@@ -7,21 +7,22 @@ import pytorch_lightning as pl
 sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), "../.."))
 from src.alias_free_gan import AliasFreeGAN
 
-# Default Alias-Free GAN Args
+class TestAliasFreeGAN():
+    
+    def test_add_model_specific_args_defaults(self):
+        parser = ArgumentParser()
+        parser = AliasFreeGAN.add_model_specific_args(parser)
+        args = parser.parse_args(['--size', '256'])
 
-parser = ArgumentParser()
-parser = AliasFreeGAN.add_model_specific_args(parser)
-args = parser.parse_args(['--size', '256'])
-
-assert args.ada_every == 256
-assert args.ada_length == 500000
-assert args.ada_target == 0.6
-assert args.argument_p == 0.0
-assert args.augment == False
-assert args.batch == 16
-assert args.d_reg_every == 16
-assert args.lr_d == 0.002
-assert args.lr_g == 0.002
-assert args.n_samples == 9
-assert args.r1 == 10.0
-assert args.size == 256
+        assert args.ada_every == 256
+        assert args.ada_length == 500000
+        assert args.ada_target == 0.6
+        assert args.argument_p == 0.0
+        assert args.augment == False
+        assert args.batch == 16
+        assert args.d_reg_every == 16
+        assert args.lr_d == 0.002
+        assert args.lr_g == 0.002
+        assert args.n_samples == 9
+        assert args.r1 == 10.0
+        assert args.size == 256
