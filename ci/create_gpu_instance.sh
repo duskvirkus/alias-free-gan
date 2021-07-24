2bd779b4f87b0c8312deab3aea739c4bdf3fb511
duskvirkus <duskvirkus@protonmail.com>
2021-07-24 06:32:10 +0000

    trying again

--- ci/create_gpu_instance.sh
+++ ci/create_gpu_instance.sh
@@ -7,7 +7,7 @@ stat=$?;
 if [ $stat -eq 0 ];then
     export USING_ZONE=us-central1-a
     echo "created on zone us-central1-a"
-    exit(0)
+    exit 0
 else
     echo "zone us-central1-a failed"
 fi
@@ -19,7 +19,7 @@ stat=$?;
 if [ $stat -eq 0 ];then
     export USING_ZONE=us-central1-b
     echo "created on zone us-central1-b"
-    exit(0)
+    exit 0
 else
     echo "zone us-central1-b failed"
 fi
@@ -31,7 +31,7 @@ stat=$?;
 if [ $stat -eq 0 ];then
     export USING_ZONE=us-central1-c
     echo "created on zone us-central1-c"
-    exit(0)
+    exit 0
 else
     echo "zone us-central1-c failed"
 fi
@@ -43,13 +43,13 @@ stat=$?;
 if [ $stat -eq 0 ];then
     export USING_ZONE=us-central1-f
     echo "created on zone us-central1-f"
-    exit(0)
+    exit 0
 else
     echo "zone us-central1-f failed"
 fi
 
 echo "no zones worked!"
-exit(1)
+exit 1
 
 # gcloud compute instances create ci-gpu-instance --source-instance-template ci-gpu-template --zone us-central1-f
 
